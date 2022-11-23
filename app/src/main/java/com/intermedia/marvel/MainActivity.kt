a630a4af0323d25297c95df7ee4e4ac3695f7b04
Chifi <esteban128@gmail.com>
2022-11-22 23:26:45 -0300

    Update dependencies

--- app/src/main/java/com/intermedia/marvel/MainActivity.kt
+++ app/src/main/java/com/intermedia/marvel/MainActivity.kt
@@ -2,8 +2,13 @@ package com.intermedia.marvel_challenge
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.intermedia.marvel_challenge.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    val model
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
